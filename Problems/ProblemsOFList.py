071a723a7bc12231f54b3f74c0aa6a1617077823
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-02-16 08:12:27 +0530

    3 problems of list solved
    hmmm solving 2 problems after weeks, realized that, it's more about solving problems.

--- Problems/ProblemsOFList.py
+++ Problems/ProblemsOFList.py
@@ -1,1 +1,29 @@
 
+#length of list in python
+list=[1,2,3,4,3,2,2,23,2,2]
+counter =0
+for item in list:
+    counter=counter+1
+print(counter)
+
+#check if element exist in list in python
+list=[21,32,32,23,35,3,2,21,22,1]
+element=2
+for item in list:
+    if item == element:
+        print("Found it!!")
+        break
+
+#reversing a list in python
+
+list=[2,32,21,23,23]
+
+list2=[0,0,0,0,0]
+print(list2,"list 1",list)
+counter=4
+for item in list:
+    list2[counter]=item
+    print(list2,list, counter)
+    counter=counter-1
+
+print(list2)
