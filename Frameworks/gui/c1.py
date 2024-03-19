f87ec472d6a2ec3d8a2dc3de60a5a1d84b294c18
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-03-19 08:04:41 +0530

    finally

--- Frameworks/gui/c1.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-root=Tk()
-root.title('My Box GUI')
-root.iconbitmap('apple.ico')
-my_label=Label(root,text="Hi")
-my_label2=Label(root,text="How you doing today?")
-
-my_label.pack()
-my_label2.pack()
