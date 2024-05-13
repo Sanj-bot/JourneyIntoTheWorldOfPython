a1544bc6444af00eb05d322b3316d644871696d4
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-05-13 22:37:29 +0530

    some final changes, finally 3rd tutorial works

--- Frameworks/Flask/flask_blog/flaskblog.py
+++ Frameworks/Flask/flask_blog/flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template,url_for
-app=Flask(__name__)
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 posts=[
     {
         'author':'Corey Schafer',
@@ -28,6 +30,20 @@ def home():
 @app.route("/about") 
 def about():
     return render_template('about.html',title='About')
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register',form=form)
+
+
+@app.route("/login")
+def login():
+    form=LoginForm()
+    return render_template('login.html',title='Login',form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
