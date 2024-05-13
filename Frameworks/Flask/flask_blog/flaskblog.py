5e5bd726ecf934fafab3e96a5fbe48fa5a0dff50
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-05-13 09:12:11 +0530

    dayOne

--- Frameworks/Flask/flask_blog/flaskblog.py
+++ Frameworks/Flask/flask_blog/flaskblog.py
@@ -4,5 +4,13 @@ app=Flask(__name__)
 #  using decorators to make different pages.
 # decorators help in adding additional functionalites.
 @app.route("/") # forward slash is the root page.
-def hello():
-    return "<h1><center>Hello World!</center></h1> <p>some content</p>"
+@app.route("/home")
+def home():
+    return "<h1>Home Page<h1>"
+
+@app.route("/about") # forward slash is the root page.
+def about():
+    return "<h1> About Page<h1>"
+
+if __name__=='__main__':
+    app.run(debug=True)
