d1d73b0ed68a341681572a299f62b08c9b0cbdba
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-05-15 22:52:23 +0530

    changes

---
+++ Frameworks/flask/main.py
@@ -0,0 +1,43 @@
+from random import randint
+import sqlite3
+from flask import Flask, jsonify
+import time
+
+#defining a flask app instance
+app = Flask(__name__)
+
+# connecting database to sql lite
+#conn for overall connection
+conn = sqlite3.connect('v_c.db')
+#cursor executes specific sql statements.
+c = conn.cursor()
+
+
+c.execute('''CREATE TABLE IF NOT EXISTS vehicle_counts (timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, auto INTEGER, car INTEGER, bus INTEGER, truck INTEGER, motorcycle INTEGER )''')
+
+def generate_counts():
+  return {
+      "auto": randint(0, 10),
+      "car": randint(1, 10),
+      "bus": randint(1, 10),
+      "truck": randint(1, 10),
+      "motorcycle": randint(1, 10),
+      
+  }
+
+
+
+
+
+while True:
+  counts = generate_counts()
+  c.execute("INSERT INTO vehicle_counts (auto, car, bus, truck, motorcycle) VALUES (?, ?, ?, ?, ?)", (counts["auto"], counts["car"], counts["bus"], counts["truck"], counts["motorcycle"]))
+  conn.commit()
+  c.execute("SELECT * FROM vehicle_counts ORDER BY timestamp DESC LIMIT 10")
+  data = c.fetchone()
+  print(f"latest: {data}")
+  
+
+if __name__ == '__main__':
+  
+  app.run(debug=True)
--- Frameworks/flask_blog/flaskblog/forms.py
+++ Frameworks/flask_blog/flaskblog/forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
+from flask_wtf.file import FileField, FileAllowed
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -13,15 +17,15 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    def validate_username(self,username):
-        
-        user=User.query.filter_by(username=username.data).first()
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('that username is taken, please choose a different username')
-    def validate_email(self,email):
-        user=User.query.filter_by(email=email.data).first()
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('that email is taken, please choose a different email')
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
@@ -30,4 +34,47 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-    
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+class RequestResetForm(FlaskForm):
+    email=StringField('Email',
+                        validators=[DataRequired(),Email()])
+    submit=SubmitField('Request Password Reset')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+                raise ValidationError('There is no account with that email. You must register first. ')
+
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit=SubmitField('Reset Password')
